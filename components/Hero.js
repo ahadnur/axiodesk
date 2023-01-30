ab7d5e6d48f4186012d063025054e5e82a4e952a
ahadnur <ahadnur44@gmail.com>
2023-01-30 16:50:43 +0600

    hero text align changed

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 function Hero() {
   return (
     <div className="bg-[url(/img/bg.svg)] w-full h-[calc(100vh-80px)] bg-right-bottom bg-no-repeat bg-[80vw_auto] 2xl:bg-auto">
-      <div className="container px-5 lg:px-16 2xl:px-80 h-[calc(90vh-80px)] w-full flex flex-col justify-center md:justify-start 2xl:justify-center items-start">
+      <div className="container px-5 lg:px-16 2xl:px-80 h-[calc(90vh-80px)] w-full flex flex-col justify-center items-start">
         <h1 className="text-4xl md:text-6xl xl:text-[80px] w-full 2xl:w-[1000px] font-bold md:mt-10 2xl:mt-0 text-gray-800">
           A leading advertising agency in Wichita Falls
         </h1>
